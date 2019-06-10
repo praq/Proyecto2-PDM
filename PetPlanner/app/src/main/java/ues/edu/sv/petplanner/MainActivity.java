03bc5ec903fbcfc160337e06cbdcd859be217d37
Paola <nenee.aguilar@gmail.com>
2019-06-10 10:03:36 -0600

    diseño del main

--- PetPlanner/app/src/main/java/ues/edu/sv/petplanner/MainActivity.java
+++ PetPlanner/app/src/main/java/ues/edu/sv/petplanner/MainActivity.java
@@ -1,7 +1,9 @@
 package ues.edu.sv.petplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void registro(View v)
+    {
+        Intent ints = new Intent(this,RegistroActivity.class);
+        startActivity(ints);
     }
 }
