296914a3318434f51f257d5f9e43baecfebc3055
Paola <nenee.aguilar@gmail.com>
2019-06-13 12:16:26 -0600

    Libreria de FB - Para logearse

--- PetPlanner/app/src/main/java/ues/edu/sv/petplanner/IniciarSesionActivity.java
+++ PetPlanner/app/src/main/java/ues/edu/sv/petplanner/IniciarSesionActivity.java
@@ -2,11 +2,32 @@ package ues.edu.sv.petplanner;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class IniciarSesionActivity extends AppCompatActivity {
 
@@ -20,12 +41,12 @@ public class IniciarSesionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_iniciar_sesion);
         usuario= (EditText) findViewById(R.id.nombreUsuario);
         contrasena = (EditText) findViewById(R.id.contraseña);
+
         helper = new DBHelper(this);
         helper.abrir();
-        //String mensaje = helper.llenarBD();
         helper.cerrar();
-        //Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
     }
+
 
     public void iniciarSesion(View v) {
         String login = usuario.getText().toString();
