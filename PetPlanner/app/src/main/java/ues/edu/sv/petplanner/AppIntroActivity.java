fe370192ef3e029792c2fbea5526c56519b723bf
Moto <mh16006@ues.edu.sv>
2019-06-16 19:21:46 -0600

    Finalizadas vistas introductorias

--- PetPlanner/app/src/main/java/ues/edu/sv/petplanner/AppIntroActivity.java
+++ PetPlanner/app/src/main/java/ues/edu/sv/petplanner/AppIntroActivity.java
@@ -2,6 +2,7 @@ package ues.edu.sv.petplanner;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -13,16 +14,37 @@ import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class AppIntroActivity extends AppIntro {
-DBHelper helper;
+    DBHelper helper;
+    final int MY_PERMISSIONS_REQUEST=124;
+    int contador=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_app_intro);
         helper= new DBHelper(this);
 
         addSlide(AppIntroFragment.newInstance("BIENVENIDO A PETPLANNER!!!","UNA APLICACION MUY UTIL E INTUITIVA QUE TE PERMITIRA ADMINISTRAR EL CUIDADO DE TU MASCOTA.",R.drawable.intro1, Color.rgb(222,86,64)));
         addSlide(AppIntroFragment.newInstance("HAY MUCHO QUE OFRECER!!!","DENTRO DE LA APLICACION PODRAS REGISTRAR A TU MASCOTA, CONTROLAR SUS VACUNAS, CONTROLAR SUS MEDICAMENTOS, REALIZAR RUTINAS Y MAS...",R.drawable.intro4, Color.rgb(244,175,73)));
+
         addSlide(AppIntroFragment.newInstance("YA FALTA POCO...","VAMOS A CONFIGURAR UNOS PERMISOS",R.drawable.intro3, Color.rgb(67,170,218)));
+        askForPermissions(new String[]{Manifest.permission.ANSWER_PHONE_CALLS}, 1);
+
+
+        showStatusBar(false);
+        setFlowAnimation();
+        //setBarColor(Color.parseColor("#333639"));
+        //setSeparatorColor(Color.parseColor("#2196F3"));
+    }
+
+    @Override
+    public void onSlideChanged(Fragment oldFragment, Fragment newFragment){
+        contador++;
+        if(contador>2){
+            pedirPermisos();
+        }
     }
 
     @Override
@@ -45,5 +67,35 @@ DBHelper helper;
         Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
         Intent inte = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(inte);
+    }
+
+    public void pedirPermisos(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(getApplicationContext(),"permission was granted, yay!",Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),"permission denied, boo!",Toast.LENGTH_LONG).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- PetPlanner/app/src/main/java/ues/edu/sv/petplanner/MainActivity.java
+++ PetPlanner/app/src/main/java/ues/edu/sv/petplanner/MainActivity.java
@@ -1,20 +1,28 @@
 package ues.edu.sv.petplanner;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     DBHelper helper;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         helper= new DBHelper(this);
+
+
+
         helper.abrir();
         if(helper.consultarIntro()==0){
             Intent inte= new Intent(this, AppIntroActivity.class);
@@ -32,4 +40,6 @@ public class MainActivity extends AppCompatActivity {
         Intent ints = new Intent(this, IniciarSesionActivity.class);
         startActivity(ints);
     }
+
+
 }
